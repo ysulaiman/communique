657a9764eda0146c8cf0c5a411441e6248429ad0
Yaser Sulaiman <yaser.a.sulaiman@gmail.com>
2012-04-21 19:14:12 +0300

    Refactor the (still trivial) Planner#solve method

--- lib/planner.rb
+++ lib/planner.rb
@@ -9,12 +9,26 @@ class Planner
   end
 
   def solve
-    applicable_action = @actions.find { |action| @initial_state.satisfy?(&action.precondition) }
-    @initial_state.instance_eval(&applicable_action.effect)
-    @plan << applicable_action if @initial_state.instance_eval(&@goal)
+    applicable_action = find_applicable_action
+    execute(applicable_action)
+    @plan << applicable_action if goal_is_satisfied?
   end
 
   def plan
     @plan.collect { |action| action.name }.join(' ')
+  end
+
+  private
+
+  def find_applicable_action
+    @actions.find { |action| @initial_state.satisfy?(&action.precondition) }
+  end
+
+  def execute(action)
+    @initial_state.apply(&action.effect)
+  end
+
+  def goal_is_satisfied?
+    @initial_state.satisfy?(&@goal)
   end
 end
