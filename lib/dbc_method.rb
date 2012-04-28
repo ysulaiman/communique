df44276fed106f1c48b74e4b641b014271036072
Yaser Sulaiman <yaser.a.sulaiman@gmail.com>
2012-04-28 23:49:30 +0300

    Remove unused methods from DbcMethod
    DbC methods are not responsible for evaluating their conditions; States
    are responsible for checking if they satisfy preconditions and for
    applying postconditions to themselves.

--- lib/dbc_method.rb
+++ lib/dbc_method.rb
@@ -8,29 +8,4 @@ class DbcMethod
     @name = name
     @parameters = parameters
   end
-
-  def evaluate_precondition
-    evaluate_condition(:pre)
-  end
-
-  def evaluate_postcondition
-    evaluate_condition(:post)
-  end
-
-  private
-
-  def evaluate_condition(type)
-    result = case type
-      when :pre
-        eval self.precondition
-      when :post
-        eval self.postcondition
-    end
-
-    if !!result == result   # If result itself is boolean
-      result
-    else
-      false
-    end
-  end
 end
