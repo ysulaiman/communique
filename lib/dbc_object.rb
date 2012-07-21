54999dc87cc9e388a616ddcc85b1c154ab476723
Yaser Sulaiman <yaser.a.sulaiman@gmail.com>
2012-07-22 00:32:58 +0300

    Make DbC objects know about their DbC methods
    As DbC objects are replacing DbC classes at the heart of State, I don't
    want--for now at least--the additional complexity of having the
    responsibility of instance variables in one class (DbcObject) and the
    responsibility of methods in another (DbcClass).

--- lib/dbc_object.rb
+++ lib/dbc_object.rb
@@ -1,9 +1,10 @@
 class DbcObject
-  attr_reader :dbc_name, :dbc_class
+  attr_reader :dbc_name, :dbc_class, :dbc_methods
 
   def initialize(dbc_name, dbc_class, dbc_instance_variables)
     @dbc_name = dbc_name
     @dbc_class = dbc_class
+    @dbc_methods = []
     initialize_dbc_instance_variables(dbc_instance_variables)
   end
 
@@ -13,6 +14,11 @@ class DbcObject
 
   def apply(&postcondition)
     instance_eval &postcondition
+  end
+
+  def add_dbc_method(method)
+    @dbc_methods << method
+    method.receiver_name = @dbc_name
   end
 
   private
--- test/test_dbc_object.rb
+++ test/test_dbc_object.rb
@@ -3,6 +3,7 @@ require_relative 'test_helpers'
 class TestDbcObject < MiniTest::Unit::TestCase
   def setup
     @dbc_object = DbcObject.new('account_instance', :Account, {:@number => 42})
+    @dbc_method = DbcMethod.new('dbc_method')
   end
 
   def test_has_dbc_name
@@ -20,5 +21,21 @@ class TestDbcObject < MiniTest::Unit::TestCase
   def test_can_apply_postconditions_to_itself
     @dbc_object.apply { @number = 666 }
     assert_equal true, @dbc_object.satisfy? { @number == 666 }
+  end
+
+  def test_has_readable_dbc_methods
+    assert_respond_to @dbc_object, :dbc_methods
+  end
+
+  def test_can_add_dbc_methods_to_itself
+    @dbc_object.add_dbc_method(@dbc_method)
+
+    assert_equal 'dbc_method', @dbc_object.dbc_methods.first.name
+  end
+
+  def test_set_receiver_name_of_dbc_methods_when_they_get_added
+    @dbc_object.add_dbc_method(@dbc_method)
+
+    assert_equal 'account_instance', @dbc_method.receiver_name
   end
 end
