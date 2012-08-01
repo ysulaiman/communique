b8fce0bb86de0e7d5db92841fd8d65777604cce2
Yaser Sulaiman <yaser.a.sulaiman@gmail.com>
2012-08-02 00:48:25 +0300

    Change UseCase to fit in the new picture of State
    Use Cases can now hold on to DbC Objects, which shall be used to set up
    the initial state of Planners.

--- lib/dbc_use_case.rb
+++ lib/dbc_use_case.rb
@@ -1,7 +1,12 @@
 class DbcUseCase
-  attr_accessor :name, :precondition, :postcondition
+  attr_accessor :name, :dbc_instances, :postcondition
 
   def initialize(name)
     @name = name
+    @dbc_instances = []
+  end
+
+  def reset_dbc_instances
+    @dbc_instances.each { |instance| instance.reset_instance_variables }
   end
 end
--- test/test_dbc_use_case.rb
+++ test/test_dbc_use_case.rb
@@ -3,15 +3,24 @@ require_relative 'test_helpers'
 class TestDbcUseCase < MiniTest::Unit::TestCase
   def setup
     @use_case = DbcUseCase.new('a_use_case')
+    @dbc_object = DbcObject.new('account_instance', :Account, {:@number => 42})
   end
 
   def test_has_name
     assert_equal 'a_use_case', @use_case.name
   end
 
-  def test_has_accessible_percondition
-    assert_respond_to @use_case, :precondition
-    assert_respond_to @use_case, :precondition=
+  def test_has_accessible_dbc_instances
+    assert_respond_to @use_case, :dbc_instances
+    assert_respond_to @use_case, :dbc_instances=
+  end
+
+  def test_can_reset_its_dbc_instances
+    @use_case.dbc_instances << @dbc_object
+    @dbc_object.number = 666
+    @use_case.reset_dbc_instances
+
+    assert_equal 42, @use_case.dbc_instances.first.number
   end
 
   def test_has_accessible_postcondition
