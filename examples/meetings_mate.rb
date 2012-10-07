e86df144da0e6e92ff945fca98fd4ca257b14775
Yaser Sulaiman <yaser.a.sulaiman@gmail.com>
2012-10-08 00:10:13 +0300

    Use new way of referring to DbcMethod parameters
    As part of the change, and as advised by Dr. Ahmed, I've made instance
    variables (and in effect, parameters) more abstract by replacing
    specific values (such as location) with boolean flags (such as
    `is_final_meeting_time_set`).

--- examples/meetings_mate.rb
+++ examples/meetings_mate.rb
@@ -17,25 +17,20 @@ vote_instance = DbcObject.new('vote', :Vote, {
 })
 
 meeting_instance = DbcObject.new('meeting', :Meeting, {
-  :@final_meeting_time => nil,
-  :@final_meeting_location => nil,
+  :@is_final_meeting_time_set => false,
+  :@is_final_meeting_location_set => false,
   :@vote => vote_instance
 })
 
 user_profile_instance = DbcObject.new('user_profile', :UserProfile, {
-  :@username => 'john',
-  :@password => 'secret',
   :@is_logged_in => true,
   :@notifications => []
 })
 
 log_in = DbcMethod.new(:log_in)
-log_in.parameters = {username: 'john', password: 'secret'}
 log_in.precondition = Proc.new do
   @dbc_class == :UserProfile &&
-    !@is_logged_in &&
-    log_in.parameters[:username] == @username &&
-    log_in.parameters[:password] == @password
+    !@is_logged_in
 end
 log_in.postcondition = Proc.new { @is_logged_in = true }
 
@@ -47,70 +42,64 @@ end
 log_out.postcondition = Proc.new { @is_logged_in = false }
 
 update_user_notifications = DbcMethod.new(:update_user_notifications)
-update_user_notifications.parameters = {notification: notification_instance}
 update_user_notifications.precondition = Proc.new do
   @dbc_class == :UserProfile &&
-    ! update_user_notifications.parameters[:notification].nil?
+    ! state.get_instance_of(:Notification).nil?
 end
 update_user_notifications.postcondition = Proc.new do
-  @notifications << update_user_notifications.parameters[:notification]
+  @notifications << state.get_instance_of(:Notification)
 end
 
-user_profile_instance.add_dbc_methods(log_in, log_out, update_user_notifications)
+user_profile_instance.add_dbc_methods(log_in, log_out,
+                                      update_user_notifications)
 
 add_notification = DbcMethod.new(:add_notification)
-add_notification.parameters = {meeting: meeting_instance}
 add_notification.precondition = Proc.new do
   @dbc_class == :Notification &&
-    ! add_notification.parameters[:meeting].nil?
-  # TODO: Change the way you use parameters in conditions.
-  # Due to the closure nature of Ruby blocks, the following condition will
-  # always refer to the objects defined in this file, never their clones that
-  # are generated and modified during (deterministic) planning. Therefore, it
-  # will always evaluate to false if the vote is initially open, the
-  # add_notification will never be applicable, and the planner will fail.
-    #add_notification.parameters[:meeting].vote.is_closed
+    ! state.get_instance_of(:Vote).nil? &&
+    state.get_instance_of(:Vote).is_closed
+  # TODO: Solve the problem of "over-cloning" to be able to write the correct
+  # condition, i.e.:
+  # state.get_instance_of(:Meeting).vote.is_closed
 end
-add_notification.postcondition = Proc.new do @meeting =
-  add_notification.parameters[:meeting] end
+add_notification.postcondition = Proc.new do
+  @meeting = state.get_instance_of(:Meeting)
+end
 
 notification_instance.add_dbc_methods(add_notification)
 
 close_vote = DbcMethod.new(:close_vote)
 close_vote.precondition = Proc.new do
   @dbc_class == :Vote &&
-    ! @is_closed
+    ! @is_closed &&
+    state.get_instance_of(:Meeting).is_final_meeting_time_set &&
+    state.get_instance_of(:Meeting).is_final_meeting_location_set
 end
 close_vote.postcondition = Proc.new { @is_closed = true }
 
 vote_instance.add_dbc_methods(close_vote)
 
 set_final_meeting_location = DbcMethod.new(:set_final_meeting_location)
-set_final_meeting_location.parameters = {location: 'Gale Crater'}
-set_final_meeting_location.precondition = Proc.new do
-  @dbc_class == :Meeting &&
-    ! set_final_meeting_location.parameters[:location].nil?
-end
+set_final_meeting_location.precondition = Proc.new { true }
 set_final_meeting_location.postcondition = Proc.new do
-  @final_meeting_location = set_final_meeting_location.parameters[:location]
+  @is_final_meeting_location_set = true
 end
 
 set_final_meeting_time = DbcMethod.new(:set_final_meeting_time)
-set_final_meeting_time.parameters = {time: Time.now}
-set_final_meeting_time.precondition = Proc.new do
-  @dbc_class == :Meeting &&
-    ! set_final_meeting_time.parameters[:time].nil?
-end
+set_final_meeting_time.precondition = Proc.new { true }
 set_final_meeting_time.postcondition = Proc.new do
-  @final_meeting_time = set_final_meeting_time.parameters[:time]
+  @is_final_meeting_time_set = true
 end
 
-meeting_instance.add_dbc_methods(set_final_meeting_location, set_final_meeting_time)
+meeting_instance.add_dbc_methods(set_final_meeting_location,
+                                 set_final_meeting_time)
 
 finalize_meeting_use_case = DbcUseCase.new('Finalize Meeting')
-finalize_meeting_use_case.dbc_instances << notification_instance << vote_instance << meeting_instance << user_profile_instance
+finalize_meeting_use_case.dbc_instances << notification_instance <<
+  vote_instance << meeting_instance << user_profile_instance
 finalize_meeting_use_case.postconditions = {
-  'meeting' => Proc.new { ! @final_meeting_time.nil? && ! @final_meeting_location.nil? },
+  'meeting' => Proc.new { @is_final_meeting_time_set &&
+    @is_final_meeting_location_set },
   'vote' => Proc.new { @is_closed },
   'notification' => Proc.new { ! @meeting.nil? },
   'user_profile' => Proc.new { @notifications.include? notification_instance }
@@ -122,4 +111,4 @@ planner.goals = finalize_meeting_use_case.postconditions
 
 puts "Solving UC #{finalize_meeting_use_case.name} ..."
 planner.solve
-puts "Solution: #{planner.plan}\n\n"
+puts "Solution: #{planner.plan}\n"
